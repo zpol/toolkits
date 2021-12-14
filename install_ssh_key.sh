31d33cb533d0bc64ad0633577048beb6e5ba4dee
Paul <zpol@users.noreply.github.com>
2021-12-15 00:47:22 +0100

    ss

--- install_ssh_key.sh
+++ install_ssh_key.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 
 echo "Installing SSH Private key"
-aws ssm get-parameter --name private_ssh_key --with-decryption --region us-west-2 | jq -r '.[].Value' | base64 -d > ~/.ssh/id_rsa
+aws ssm get-parameter --name private_ssh_key --with-decryption --region us-west-2 | jq -r '.[].Value' | base64 -d > /tmp/id_rsa
