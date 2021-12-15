55f1046473f8c164514dd65ea0dfdc29d46e532f
Paul <zpol@users.noreply.github.com>
2021-12-15 08:27:00 +0100

    trick to copy the key in the proper location

--- install_ssh_key.sh
+++ install_ssh_key.sh
@@ -2,3 +2,4 @@
 
 echo "Installing SSH Private key"
 aws ssm get-parameter --name private_ssh_key --with-decryption --region us-west-2 | jq -r '.[].Value' | base64 -d > /tmp/id_rsa
+cp /rmp/id_rsa /home/ec2/.ssh/
