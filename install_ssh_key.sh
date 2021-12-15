95fc68bfa34111cf23edc817c307e3761affcf95
Paul <zpol@users.noreply.github.com>
2021-12-15 19:45:45 +0100

    copy public key too

--- install_ssh_key.sh
+++ install_ssh_key.sh
@@ -3,3 +3,7 @@
 echo "Installing SSH Private key"
 aws ssm get-parameter --name private_ssh_key --with-decryption --region us-west-2 | jq -r '.[].Value' | base64 -d > /tmp/id_rsa
 cp /tmp/id_rsa /home/ec2-user/.ssh/
+
+echo "Installing SSH Public key"
+aws ssm get-parameter --name public_ssh_key --with-decryption --region us-west-2 | jq -r '.[].Value' | base64 -d > /tmp/id_rsa.pub
+cp /tmp/id_rsa.pub /home/ec2-user/.ssh/
