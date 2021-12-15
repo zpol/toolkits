700730ae628fba16927982e0ceb0b0c1fad06318
Paul <zpol@users.noreply.github.com>
2021-12-15 19:53:13 +0100

    quick fix

--- install_ssh_key.sh
+++ install_ssh_key.sh
@@ -5,5 +5,5 @@ aws ssm get-parameter --name private_ssh_key --with-decryption --region us-west-
 cp /tmp/id_rsa /home/ec2-user/.ssh/
 
 echo "Installing SSH Public key"
-aws ssm get-parameter --name public_ssh_key --with-decryption --region us-west-2 | jq -r '.[].Value' | base64 -d > /tmp/id_rsa.pub
+aws ssm get-parameter --name public_ssh_key --with-decryption --region us-west-2 | jq -r '.[].Value' > /tmp/id_rsa.pub
 cp /tmp/id_rsa.pub /home/ec2-user/.ssh/
