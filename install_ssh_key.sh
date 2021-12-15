951bd53a751b693f023dccdca21b2a4733ad29f4
Paul <zpol@users.noreply.github.com>
2021-12-15 09:04:43 +0100

    wee

--- install_ssh_key.sh
+++ install_ssh_key.sh
@@ -2,4 +2,4 @@
 
 echo "Installing SSH Private key"
 aws ssm get-parameter --name private_ssh_key --with-decryption --region us-west-2 | jq -r '.[].Value' | base64 -d > /tmp/id_rsa
-cp /rmp/id_rsa /home/ec2/.ssh/
+cp /tmp/id_rsa /home/ec2-user/.ssh/
